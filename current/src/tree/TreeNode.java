787c099ababf35f950b5e76eebc58f75f117a90c
zhangsan <zhangsan@163.com>
2019-11-10 21:41:19 -0400

    树的结构

---
+++ current/src/tree/TreeNode.java
@@ -0,0 +1,16 @@
+package tree;
+
+/**
+ * @author Zhangkj
+ * @date 2019-11-10-20:06
+ */
+public class TreeNode {
+
+    TreeNode left;
+    TreeNode right;
+    int data;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+}
