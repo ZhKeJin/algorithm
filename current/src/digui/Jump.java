70830a60c31ce3e4fb9636733fb57cece029c52c
zhangsan <zhangsan@163.com>
2019-11-19 10:58:02 +0800

    爬楼梯

--- current/src/digui/Jump.java
+++ current/src/digui/Jump.java
@@ -4,12 +4,17 @@ package digui;
  * @author Zhangkj
  * @date 2019-11-13-20:41
  */
-public class Test1 {
+
+/**
+ * 爬楼梯
+ */
+public class Jump {
 
     public static void main(String[] args) {
 
 
         System.out.println(fun(3));
+        System.out.println(fun2(5));
 
     }
 
@@ -26,6 +31,24 @@ public class Test1 {
 
         return fun(n-1)+fun(n-2);
 
+    }
+
+
+    static int fun2(int n){
+
+        int i=1;
+        int j=2,t;
+
+
+        int k=3;
+        while(k<=n){
+            t=j;
+            j=i+j;
+            k++;
+            i=t;
+        }
+
+        return j;
     }
 
 
