e87ca4b90d4766a5a8edc277adb6d319b5ec3c55
ZhKeJin <3280221076@qq.com>
2019-12-15 21:45:48 +0800

    请找出这个两个只出现一次的数字

--- jianzhi/src/array/Jz56.java
+++ jianzhi/src/array/Jz56.java
@@ -5,6 +5,8 @@ package array;
  * @date 2019-12-13-21:19
  */
 
+import java.util.IntSummaryStatistics;
+
 /**
  * 一个整型数组里除两个数字之外，其他数字都出现了两次。
  * 请找出这个两个只出现一次的数字，时间复杂度O(n),空间复杂度O(1)     数组中只出现一次的数字
@@ -49,6 +51,33 @@ public class Jz56 {
             num1[0] = result1;
             num2[0] = result2;
         }
+
+        public void FindNumsAppearOnce_1(int [] array,int num1[] , int num2[]) {
+
+            int index =0;
+
+            for (int i = 0; i < array.length; i++) {
+                index = index ^array[i];
+            }
+
+            int e = 1;
+            while((index&1)==0){
+                e = e<<1;
+            }
+
+            int result1 = 0;
+            int result2 = 0;
+            for (int i = 0; i < array.length; i++) {
+
+                if((array[i]^e)==0){
+                    result1^=array[i];
+                } else{
+                    result2^=array[i];
+                }
+            }
+
+        }
+
 
 
 
