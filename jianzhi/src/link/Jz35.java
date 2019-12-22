4e15e1ffd87b688441224e881a7f9342eb0f9df3
ZhKeJin <3280221076@qq.com>
2019-12-22 19:43:27 +0800

    复杂链表的复制

--- jianzhi/src/link/Jz35.java
+++ jianzhi/src/link/Jz35.java
@@ -37,8 +37,10 @@ public class Jz35 {
         if(head == null){
             return head;
         }
+
         // map方法，空间复杂度O(n)
         Node node = head;
+
         // 使用hash表存储旧结点和新结点的映射
         Map<Node,Node> map = new HashMap<>();
         while(node != null){
@@ -46,7 +48,9 @@ public class Jz35 {
             map.put(node,clone);
             node = node.next;
         }
+
         node = head;
+
         while(node != null){
             map.get(node).next = map.get(node.next);
             map.get(node).random = map.get(node.random);
@@ -55,12 +59,19 @@ public class Jz35 {
         return map.get(head);
     }
 
+
+
+
+
+
     public Node copyRandomList_1(Node head) {
         if(head == null){
             return head;
         }
+
         // 空间复杂度O(1)，将克隆结点放在原结点后面
         Node node = head;
+
         // 1->2->3  ==>  1->1'->2->2'->3->3'
         while(node != null){
             Node clone = new Node(node.val,node.next,null);
@@ -68,6 +79,8 @@ public class Jz35 {
             node.next = clone;
             node = temp;
         }
+
+
         // 处理random指针
         node = head;
         while(node != null){
@@ -75,6 +88,8 @@ public class Jz35 {
             node.next.random = node.random == null ? null : node.random.next;
             node = node.next.next;
         }
+
+
         // 还原原始链表，即分离原链表和克隆链表
         node = head;
         Node cloneHead = head.next;
@@ -83,6 +98,8 @@ public class Jz35 {
             node.next = node.next.next;
             node = temp;
         }
+
+
         return cloneHead;
     }
 }
