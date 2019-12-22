43ca23148ab0b1d40187a85a2f41ea9ca70a0bf5
ZhKeJin <3280221076@qq.com>
2019-12-22 19:31:32 +0800

    链表的指定k  翻转

---
+++ jianzhi/src/link/LC25.java
@@ -0,0 +1,49 @@
+package link;
+
+/**
+ * @author Zhangkj
+ * @date 2019-12-22-19:22
+ */
+
+/*
+链表的指定k  翻转
+ */
+
+
+public class LC25 {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode pre = dummy;
+        ListNode end = dummy;
+
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) end = end.next;
+            if (end == null) break;
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+
+}
