5b1eec0a394b7df9dd72c8aa73721f7b0126a702
ZhKeJin <3280221076@qq.com>
2019-12-23 20:28:57 +0800

    由 二叉搜索树 变换  为   双向链表

--- jianzhi/src/link/Jz36.java
+++ jianzhi/src/link/Jz36.java
@@ -27,16 +27,22 @@ public class Jz36 {
     public TreeNode ConvertBSTToBiList(TreeNode root) {
         if(root==null)
             return null;
+
         Stack<TreeNode> stack = new Stack<TreeNode>();
+
         TreeNode p = root;
         TreeNode pre = null;// 用于保存中序遍历序列的上一节点
+
         boolean isFirst = true;
+
         while(p!=null||!stack.isEmpty()){
             while(p!=null){
                 stack.push(p);
                 p = p.left;
             }
+
             p = stack.pop();
+
             if(isFirst){
                 root = p;// 将中序遍历序列中的第一个节点记为root
                 pre = root;
