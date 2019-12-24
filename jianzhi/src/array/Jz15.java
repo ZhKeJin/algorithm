0c65025b407c33d92b9ac12042c617a74401aa40
ZhKeJin <3280221076@qq.com>
2019-12-24 22:52:06 +0800

    数中的二进制中的1 的个数

--- jianzhi/src/array/Jz15.java
+++ jianzhi/src/array/Jz15.java
@@ -5,6 +5,8 @@ package array;
  * @date 2019-12-15-22:28
  */
 
+import com.sun.javaws.jnl.InformationDesc;
+
 import javax.swing.*;
 import java.util.concurrent.CountDownLatch;
 
@@ -16,12 +18,45 @@ public class Jz15 {
 
     public static void main(String[] args) {
 
-        int i = new Jz15().NumberOf1(5);
+        int i = new Jz15().NumberOf1(7);
+        int j = new Jz15().NumberOf2(7);
+        System.out.println(j);
 
         System.out.println(i);
     }
 
-    public int NumberOf1(int n) {
+
+    public int NumberOf2(int n) {
+
+        int index = 1;
+
+        int count =0;
+        while(index!=0){
+
+            if((n&index )!=0){
+                count++;
+            }
+            index = index<<1;
+        }
+
+        return count;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public int NumberOf1(int n) {
 
 
         int index = 1;
