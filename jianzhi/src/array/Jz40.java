8e31071ef1971cc8918e7fb740866ed4e7a3d37e
ZhKeJin <3280221076@qq.com>
2019-12-24 22:54:47 +0800

    最小的K个数

--- jianzhi/src/array/Jz40.java
+++ jianzhi/src/array/Jz40.java
@@ -1,5 +1,7 @@
 package array;
 
+import javax.swing.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.PriorityQueue;
@@ -17,7 +19,8 @@ public class Jz40 {
 
 
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new Jz40().GetLeastNumbers_Solution(new int[]{2, 3, 5, 1, 9, 6, 4, 0}, 4);
+//        ArrayList<Integer> arrayList = new Jz40().GetLeastNumbers_Solution(new int[]{2, 3, 5, 1, 9, 6, 4, 0}, 4);
+        ArrayList<Integer> arrayList = new Jz40().GetLeastNumbers_Solution_1(new int[]{2, 3, 5, 1, 9, 6, 4, 0}, 4);
 
         for (Integer integer : arrayList) {
             System.out.println(integer);
@@ -52,6 +55,87 @@ public class Jz40 {
             result.add(integer);
         }
         return result;
+    }
+
+    public ArrayList<Integer> GetLeastNumbers_Solution_1(int[] input, int k) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        int sort1 = sort1(input, 0, input.length - 1, k);
+
+        System.out.println(sort1);
+
+        for (int i : input) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+
+       /* if(sort1==-1){
+            return null;
+        }*/
+
+        for (int i = 0; i < k; i++) {
+            arrayList.add(input[i]);
+        }
+
+
+
+
+        return arrayList;
+
+    }
+
+
+    private int sort1(int[] arr, int left, int right,int zmid) {
+
+        if(left>=right){
+            return -1;
+        }
+
+
+        int l = left;
+        int r = right;
+
+        int s = arr[l];
+
+        while(l<r){
+
+            while(l<r&&arr[r]>=s){
+                r--;
+            }
+            while(l<r&&arr[l]<=s){
+                l++;
+            }
+
+            if(l<r){
+                int temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+            }
+        }
+
+
+
+        arr[left] = arr[l];
+        arr[l] = s;
+        if(zmid-1 == l){
+            return s;
+        }
+        if(l>zmid-1){
+
+            int p  = sort1(arr,left, l-1,zmid);
+
+            return p;
+        }else{
+
+            int o = sort1(arr, l+1, right,zmid);
+
+            return o;
+        }
+
+
+
     }
 }
 
