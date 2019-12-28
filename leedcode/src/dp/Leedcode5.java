ede7bb70dea47bdbf8a427110e0e6214875b16b1
ZhKeJin <3280221076@qq.com>
2019-12-28 20:46:34 +0800

    最长回文子串

--- leedcode/src/dp/Leedcode5.java
+++ leedcode/src/dp/Leedcode5.java
@@ -10,11 +10,29 @@ package dp;
  */
 public class Leedcode5 {
 
+    public static void main(String[] args) {
+
+        Leedcode5 leedcode5 = new Leedcode5();
+
+        String s = leedcode5.longestPalindrome("abc435cba");
+
+        System.out.println(s);
+
+
+        System.out.println("...................");
+
+
+        String string = leedcode5.getString("ababcdefacb", "acaabcdefcab");
+
+        System.out.println(string);
+
+    }
+
     public String longestPalindrome(String s) {
         if (s.equals(""))
             return "";
         String origin = s;
-        String reverse = new StringBuffer(s).reverse().toString();
+        String reverse = new StringBuffer(s).reverse().toString(); //字符串倒置
         int length = s.length();
         int[][] arr = new int[length][length];
         int maxLen = 0;
@@ -28,18 +46,61 @@ public class Leedcode5 {
                         arr[i][j] = arr[i - 1][j - 1] + 1;
                     }
                 }
-                /**********修改的地方*******************/
                 if (arr[i][j] > maxLen) {
-                    int beforeRev = length - 1 - j;
-                    if (beforeRev + arr[i][j] - 1 == i) { //判断下标是否对应
-                        maxLen = arr[i][j];
-                        maxEnd = i;
-                    }
-                    /*************************************/
+                    maxLen = arr[i][j];
+                    maxEnd = i; //以 i 位置结尾的字符
                 }
+
             }
+
         return s.substring(maxEnd - maxLen + 1, maxEnd + 1);
     }
+
+
+
+
+    public String getString(String s1,String s2){
+
+        int[][] ints = new int[s1.length()][s2.length()];
+
+        int maxlen =0;
+        int maxindex = 0;
+
+        for (int i = 0; i < s1.length(); i++) {
+
+            for (int j = 0; j < s2.length(); j++) {
+
+                if(s1.charAt(i) == s2.charAt(j)){
+
+                    if(i==0 || j==0){
+
+                        ints[i][j] = 1;
+                    }else{
+
+                        ints[i][j] = ints[i-1][j-1]+1;
+
+//                        maxlen = Math.max(ints[i][j],maxlen);
+//
+//                        maxindex = i;
+                    }
+                }
+
+                if(ints[i][j]>maxlen){
+                    maxlen = ints[i][j];
+                    maxindex = i;
+                }
+
+            }
+
+        }
+
+        return s1.substring(maxindex-maxlen+1,maxindex+1);
+
+
+
+    }
+
+
 
 
 }
