3803d6af55c6a4dd7d7131d353fe172b5e00d053
ZhKeJin <3280221076@qq.com>
2020-01-01 23:14:11 +0800

    继承

---
+++ leedcode/src/Parent.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * @author Zhangkj
+ * @date 2019-12-31-9:21
+ */
+public class Parent {
+
+
+    public void doSome()throws IOException, SQLException {
+
+
+    }
+}
+
+ class Chilen extends Parent {
+
+     public void doSome(){
+
+     }
+
+
+}
